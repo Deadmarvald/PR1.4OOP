05e753f2524c4d831a2ddd66896c452cd34f9ee9
unknown <deadmarvald@gmail.com>
2022-03-29 22:24:35 +0300

    Лабораторна робота №1.4 ООП

---
+++ PR1.4(2.0)/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+#include <stdio.h>
+#include "HomeLib.h"
+
+using namespace std;
+
+int main()
+{
+    HomeLib a[10];
+    int x = 0;
+
+    cout << "Enter the size of the library: ";
+    cin >> x;
+    cout << "Enter your books:\n";
+    a[x].Read(a, x);
+
+    system("pause");
+    return 0;
+}
---
+++ UT1.4/UT1.4.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR1.4(2.0)/HomeLib.h" 
+#include "../PR1.4(2.0)/HomeLib.cpp" 
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UT14
+{
+	TEST_CLASS(UT14)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			HomeLib t[10];
+			int x = 0;
+
+		Assert::IsTrue(t[x].Display(t, x));
+		}
+	};
+}
